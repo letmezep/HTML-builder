243e529c3ddfa02b5c184bb032d0ce00c6047bbf
letmezep <letmezep@gmail.com>
2024-01-21 01:21:22 +0800

    feat: complete 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+let pathP = path.join(__dirname, '../02-write-file/text.txt');
+
+const fs = require('fs');
+const writeStream = fs.createWriteStream(pathP);
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Hello! Please enter the text: ');
+
+rl.on('line', (input) => {
+  writeStream.write(input);
+  if (input === 'exit') {
+    rl.close();
+  }
+});
+
+process.on('exit', () => console.log('Goodbay! Text was written in text.txt'));
