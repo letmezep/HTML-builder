bce231ba4c1d24ee4ef426cc742b63585d29c649
letmezep <letmezep@gmail.com>
2024-01-22 00:34:37 +0800

    feat: complete 03 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-// import fs from 'fs/promises';
 const path = require('path');
 
 const dirPath = path.join(__dirname, './secret-folder');
@@ -9,11 +8,8 @@ fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
     console.log('Error in reading contents');
     console.log(error.message);
   } else {
-    // let out = '';
     files.forEach((element) => {
       if (element.isFile()) {
-        // console.log(element.name);
-        // const elementPath = './secret-folder/' + element.name;
         const elementPath = path.resolve(
           __dirname,
           './secret-folder/' + element.name,
@@ -21,147 +17,17 @@ fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
         const extFile = path.extname(elementPath);
         const newExtFile = path.extname(elementPath).substring(1);
         const nameFile = path.basename(elementPath, extFile);
-        // const fileInfo = fs.stat(elementPath, (err, stats) => {
-        const fileSize = fs.stat(elementPath, (err, stats) => {
+
+        fs.stat(elementPath, (err, stats) => {
           if (err) {
             console.log(err);
           } else {
-            // console.log('STATS: ', stats.size);
-            console.log(nameFile + ' ' + newExtFile + ' ' + stats.size);
+            console.log(
+              nameFile + ' - ' + newExtFile + ' - ' + stats.size + 'b',
+            );
           }
         });
-
-        // console.log(nameFile + ' ' + newExtFile);
-        // console.log(fileSize);
-
-        // console.log(path.basename(elementPath, 'csv'));
       }
     });
-    // console.log(out);
-    // console.log('Contents are..');
-    // console.log(files);
   }
 });
-
-// let allFiles = fs.readdirSync(dirPath, { withFileTypes: true });
-// // let allFiles = await fs.readdirSync(dirPath, { withFileTypes: true });
-// console.log(allFiles);
-
-// try {
-//   let allFiles = await fs.readdir(dirPath, { withFileTypes: true });
-// } catch (err) {
-//   console.log(err);
-// }
-// console.log(allFiles);
-
-// // const { error } = require('console');
-// const fs = require('fs');
-// // import { readdir } from 'fs';
-
-// const path = require('path');
-// const dirPath = path.join(__dirname, './secret-folder');
-
-// // const isFile = (fileName) => {
-// //   return fs.lstatSync(fileName).isFile();
-// // };
-
-// // fs.readdirSync(dirPath)
-// //   .map((fileName) => {
-// //     return fs.path.join(dirPath, fileName);
-// //   })
-// //   .filter(isFile);
-
-// // const isFile = (fileName) => {
-// //   if (fs.lstat(fileName).isFile()) {
-// //     console.log(fileName);
-// //   }
-// //   return fs.lstat(fileName).isFile();
-// // };
-
-// fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
-//   if (error) {
-//     console.log('Error in reading contents');
-//     console.log(error.message);
-//   } else {
-//     console.log('Contents are..');
-//     console.log(files);
-//   }
-// });
-
-// fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
-//     if (error) {
-//       console.log('Error in reading contents');
-//       console.log(error.message);
-//     } else if(files.) {
-//       console.log('Contents are..');
-//       console.log(files);
-//     }
-//   });
-
-// console.log('DIRENT: ', fs.Dirent);
-// //   .map((fileName) => {
-// //     return fs.path.join(dirPath, fileName);
-// //   })
-// //   .filter(isFile);
-
-// // fs.readdir(dirPath, { withFileTypes: true })
-// //   .map((fileName) => {
-// //     return fs.path.join(dirPath, fileName);
-// //   })
-// //   .filter(isFile);
-
-// // // console.log('Path to DIR: ', pathP);
-
-// // fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
-// //   if (error) {
-// //     console.log('Error in reading contents');
-// //     console.log(error.message);
-// //   } else if (files.isFile()) {
-// //     // fs.Dirent.filter((d) => d.isFile());
-// //     // fs.Dirent.map((d) => d.name);
-// //     console.log('Contents are..');
-// //     console.log(files);
-// // console.log('d.name: ', d.name);
-// //   }
-// // });
-
-// // try {
-// //     const files = await readdir(path);
-// //     for (const file of files)
-// //     console.log(file);
-// // } catch (err) {
-// //     console.error(err);
-// // }
-
-// // const fs = require('fs');
-// // const writeStream = fs.createWriteStream(pathP);
-
-// // const readline = require('readline');
-// // const rl = readline.createInterface({
-// //   input: process.stdin,
-// //   output: process.stdout,
-// // });
-
-// // console.log('Hello! Please enter the text: ');
-
-// // rl.on('line', (input) => {
-// //   writeStream.write(input);
-// //   if (input === 'exit') {
-// //     rl.close();
-// //   }
-// // });
-
-// // process.on('exit', () => console.log('Goodbye! Text was written in text.txt'));
-
-// // const fs = require('fs');
-// // let path = require('path');
-
-// // let pathP = path.join(__dirname, '../01-read-file/text.txt');
-
-// // const readStream = fs.createReadStream(pathP, {
-// //   encoding: 'utf8',
-// // });
-
-// // readStream.on('data', (chunk) => {
-// //   console.log(chunk.toString());
-// // });
