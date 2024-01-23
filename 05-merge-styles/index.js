ea74a096472b62a4dcedbaaf6d0bdd2d2163d0f8
letmezep <letmezep@gmail.com>
2024-01-23 14:22:24 +0800

    feat: add write bundle file in task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -12,24 +12,62 @@ let readDirectory = () => {
         if (element.isFile()) {
           const elementPath = path.resolve(__dirname, dirPath + element.name);
           const extFile = path.extname(elementPath);
-          const newExtFile = path.extname(elementPath).substring(1);
-          const nameFile = path.basename(elementPath, extFile);
+          //   const newExtFile = path.extname(elementPath).substring(1);
+          //   const nameFile = path.basename(elementPath, extFile);
           // elementPath - add folder path
           if (extFile === '.css') {
-            fs.stat(elementPath, (err, stats) => {
-              if (err) {
-                console.log(err);
-              } else {
-                console.log(
-                  nameFile + ' - ' + newExtFile + ' - ' + stats.size + 'b',
-                );
-              }
-            });
+            readFile(elementPath);
+            // const readStream = fs.createReadStream(elementPath, {
+            //   encoding: 'utf8',
+            // });
+
+            // let writeStream = fs.createWriteStream(
+            //   './05-merge-styles/project-dist/bundle.css',
+            // );
+
+            // readStream.on('data', (chunk) => {
+            //   //   console.log(chunk.toString());
+            //   //   writeStream.write(chunk);
+            //   writeBundle(chunk);
+            // });
+
+            // fs.stat(elementPath, (err) => {
+            //   if (err) {
+            //     console.log(err);
+            //   }
+            // });
           }
         }
       });
     }
   });
 };
+
+let readFile = (elementPath) => {
+  const readStream = fs.createReadStream(elementPath, {
+    encoding: 'utf8',
+  });
+  readStream.on('data', (chunk) => {
+    console.log(chunk.toString());
+    //   writeStream.write(chunk);
+    writeBundle(elementPath, chunk);
+  });
+};
+
+let writeBundle = (elementPath, chunk) => {
+  fs.appendFile('./05-merge-styles/project-dist/bundle.css', chunk, (err) => {
+    if (err) throw err;
+  });
+};
+
+// const writeBundle = () => {
+//   let writeStream = fs.createWriteStream(
+//     './05-merge-styles/project-dist/bundle.css',
+//   );
+
+//   readStream.on('data', (chunk) => {
+//     writeStream.write(chunk);
+//   });
+// };
 
 readDirectory();
