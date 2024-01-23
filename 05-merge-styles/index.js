955a330cffdd9e01d1dd675244a8b4877183c095
letmezep <letmezep@gmail.com>
2024-01-23 12:13:14 +0800

    init: start task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+let path = require('path');
+
+let readDirectory = () => {
+  fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
+    if (error) {
+      console.log('Error in reading contents');
+      console.log(error.message);
+    } else {
+      files.forEach((element) => {
+        if (element.isFile()) {
+          const elementPath = path.resolve(
+            __dirname,
+            './secret-folder/' + element.name,
+          );
+          const extFile = path.extname(elementPath);
+          const newExtFile = path.extname(elementPath).substring(1);
+          const nameFile = path.basename(elementPath, extFile);
+
+          fs.stat(elementPath, (err, stats) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(
+                nameFile + ' - ' + newExtFile + ' - ' + stats.size + 'b',
+              );
+            }
+          });
+        }
+      });
+    }
+  });
+};
