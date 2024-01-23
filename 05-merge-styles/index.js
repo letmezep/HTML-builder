8e63e84c975294a43d3378d314275df5df56c75c
letmezep <letmezep@gmail.com>
2024-01-23 14:38:39 +0800

    feat: complete task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,3 +1,4 @@
+// const { error } = require('console');
 const fs = require('fs');
 let path = require('path');
 const dirPath = path.join(__dirname, './styles/');
@@ -12,30 +13,8 @@ let readDirectory = () => {
         if (element.isFile()) {
           const elementPath = path.resolve(__dirname, dirPath + element.name);
           const extFile = path.extname(elementPath);
-          //   const newExtFile = path.extname(elementPath).substring(1);
-          //   const nameFile = path.basename(elementPath, extFile);
-          // elementPath - add folder path
           if (extFile === '.css') {
             readFile(elementPath);
-            // const readStream = fs.createReadStream(elementPath, {
-            //   encoding: 'utf8',
-            // });
-
-            // let writeStream = fs.createWriteStream(
-            //   './05-merge-styles/project-dist/bundle.css',
-            // );
-
-            // readStream.on('data', (chunk) => {
-            //   //   console.log(chunk.toString());
-            //   //   writeStream.write(chunk);
-            //   writeBundle(chunk);
-            // });
-
-            // fs.stat(elementPath, (err) => {
-            //   if (err) {
-            //     console.log(err);
-            //   }
-            // });
           }
         }
       });
@@ -49,25 +28,18 @@ let readFile = (elementPath) => {
   });
   readStream.on('data', (chunk) => {
     console.log(chunk.toString());
-    //   writeStream.write(chunk);
-    writeBundle(elementPath, chunk);
+    writeBundle(chunk);
   });
 };
 
-let writeBundle = (elementPath, chunk) => {
+let writeBundle = (chunk) => {
   fs.appendFile('./05-merge-styles/project-dist/bundle.css', chunk, (err) => {
     if (err) throw err;
   });
 };
 
-// const writeBundle = () => {
-//   let writeStream = fs.createWriteStream(
-//     './05-merge-styles/project-dist/bundle.css',
-//   );
-
-//   readStream.on('data', (chunk) => {
-//     writeStream.write(chunk);
-//   });
-// };
-
+fs.unlink('./05-merge-styles/project-dist/bundle.css', (error) => {
+  if (error) return console.log(error);
+  console.log('File DELETE');
+});
 readDirectory();
