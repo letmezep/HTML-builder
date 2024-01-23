34241e9b0f2dce0a290c1f6888831def22a34304
letmezep <letmezep@gmail.com>
2024-01-23 20:20:07 +0800

    feat: add change tags function in 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-// const path = require('path');
+const path = require('path');
 const pathDir = './06-build-page/project-dist/';
 const pathTemplate = './06-build-page/template.html';
+const pathComponents = './06-build-page/components/';
 const pathHtml = pathDir + 'index.html';
 let changeTemplate = '';
 
@@ -16,14 +17,51 @@ async function createDirectory() {
   return res;
 }
 
+async function readDirectory() {
+  const res = await fs.readdir(
+    pathComponents,
+    { withFileTypes: true },
+    (error, files) => {
+      if (error) {
+        console.log('Error in reading contents');
+        console.log(error.message);
+      } else {
+        changeTag(files);
+        // change tags
+        // files.forEach((element) => {
+        //   const elementPath = path.resolve(__dirname, pathComponents + element.name);
+        //   const copyElementPath = path.resolve(
+        //     __dirname,
+        //     copyDirPath + element.name,
+        //   );
+
+        // });
+      }
+    },
+  );
+  return res;
+}
+
+let changeTag = (files) => {
+  files.forEach((element) => {
+    const elementPath = path.resolve(__dirname, pathComponents + element.name);
+    const extFile = path.extname(elementPath);
+    const nameFile = path.basename(elementPath, extFile);
+    console.log('element.name: ', nameFile);
+    console.log('elementPath: ', elementPath);
+  });
+};
+
 let readFile = () => {
   const readStream = fs.createReadStream(pathTemplate, {
     encoding: 'utf8',
   });
   readStream.on('data', (chunk) => {
-    // console.log(chunk.toString());
-
     if (chunk.includes('{{header}}' || '{{articles}}' || '{{footer}}')) {
+      readDirectory();
+      // read directory with components
+      // regExr - file name
+      // foreach elements read file, replace tags
       const changeTemplateHeader = chunk.replace(
         '{{header}}',
         'WOODY WOODPECKER ONE header',
